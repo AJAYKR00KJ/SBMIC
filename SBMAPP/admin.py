e85ffecbbf21273dcb05ace53efedd3c6199ac80
AJAY KUMAR <48375037+AJAYKR00KJ@users.noreply.github.com>
2021-01-08 09:33:31 +0530

    added footer models

--- SBMAPP/admin.py
+++ SBMAPP/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Carousel, Implinks, Marque, Welcome
+from .models import Carousel, Implinks, Marque, Welcome, Youtube, Announcement, Tender, Events, Notice, Footer
 admin.site.register(Carousel)
 admin.site.register(Implinks)
 admin.site.register(Marque)
 admin.site.register(Welcome)
+admin.site.register(Youtube)
+admin.site.register(Tender)
+admin.site.register(Announcement)
+admin.site.register(Events)
+admin.site.register(Notice)
+admin.site.register(Footer)
