29e8f1f77da139eedb02e894baf02f2145de186d
AJAY KUMAR <48375037+AJAYKR00KJ@users.noreply.github.com>
2021-01-08 09:35:42 +0530

    updated models adding footer

--- SBMAPP/models.py
+++ SBMAPP/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Carousel(models.Model):
-    title = models.CharField(max_length = 100)
+    title = models.CharField(max_length=100)
     thumbnail = models.ImageField()
     featured = models.BooleanField()
 
@@ -30,11 +31,68 @@ class Marque(models.Model):
     def __str__(self):
         return self.link_text
 
+
 class Welcome(models.Model):
-    title= models.CharField(max_length=20)
-    text= models.TextField()
+    title = models.CharField(max_length=20)
+    text = models.TextField()
     date = models.DateTimeField()
     featured = models.BooleanField()
 
     def __str__(self):
         return self.title
+
+
+class Youtube(models.Model):
+    title = models.CharField(max_length=20)
+    date = models.DateTimeField()
+    featured = models.BooleanField()
+    link_url = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Announcement(models.Model):
+    title = models.CharField(max_length=20)
+    date = models.DateTimeField()
+    featured = models.BooleanField()
+    link_url = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Tender(models.Model):
+    title = models.CharField(max_length=20)
+    date = models.DateTimeField()
+    featured = models.BooleanField()
+    link_url = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Notice(models.Model):
+    title = models.CharField(max_length=20)
+    date = models.DateTimeField()
+    featured = models.BooleanField()
+    link_url = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Events(models.Model):
+    title = models.CharField(max_length=20)
+    date = models.DateTimeField()
+    featured = models.BooleanField()
+    link_url = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Footer(models.Model):
+    college = models.CharField(max_length=40, )
+    motive = models.CharField(max_length=100)
+    address_txt = models.TextField()
+    address_link = models.URLField()
+    mobile = models.CharField(max_length=13)
+    email = models.EmailField()
+
--- SBMAPP/views.py
+++ SBMAPP/views.py
@@ -1,19 +1,49 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import Carousel, Implinks, Marque, Welcome
+from .models import Carousel, Implinks, Marque, Welcome, Youtube, Announcement, Notice, Events, Tender, Footer
+from message.models import Message
+
 
 def home(request):
     car_img = Carousel.objects.filter(featured=True)
-    imp_links = Implinks.objects.filter(featured= True)
+    imp_links = Implinks.objects.filter(featured=True)
     mar_links = Marque.objects.filter(featured=True)
-    welcome= Welcome.objects.filter(featured=True).order_by('-date')[0:1]
+    welcome = Welcome.objects.filter(featured=True).order_by('-date')[0:1]
+    youtube = Youtube.objects.filter(featured=True).order_by('-date')[0:1]
+
+    announcement = Announcement.objects.filter(featured=True).order_by('-date')[0:8]
+    notice = Notice.objects.filter(featured=True).order_by('-date')[0:8]
+    events = Events.objects.filter(featured=True).order_by('-date')[0:8]
+    tender = Tender.objects.filter(featured=True).order_by('-date')[0:8]
+
+    footer = Footer.objects.all()
+
+    if request.method == 'POST':
+        email = request.POST["email"]
+        text = request.POST["message"]
+        new_message = Message()
+        new_message.email = email
+        new_message.text_message = text
+
+        new_message.save()
+        messages.success(request, 'Submitted')
+        return HttpResponseRedirect('/')
+
     context = {
         'object_list': car_img,
         'important_links': imp_links,
         'marque_links': mar_links,
-        'welcome': welcome
+        'welcome': welcome,
+        'Youtube': youtube,
+        'Announcement': announcement,
+        'Notice': notice,
+        'Event': events,
+        'Tender': tender,
+        'Footer': footer,
     }
-    return render(request,'home.html',context)
+    return render(request, 'home.html', context)
+
 
 def gallery(request):
-    return render(request,'gallery.html',{})
-
+    return render(request, 'gallery.html', {})
